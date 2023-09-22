1235297053f1f06caa136c96ee7d07de2fcf60f6
Marco Pellegrino <marco.pellegrino@tii.ae>
2023-09-22 20:58:35 +0400

    Dunno

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import {List} from "./list";
-import {Sidebar} from "./sidebar";
-import {Hero} from "./hero";
-import {GameProvider} from "./context"
-import "./globalStyles.css";
+import {List} from "./components/list";
+import {Sidebar} from "./components/sidebar";
+import {Hero} from "./components/hero";
+import {GameProvider} from "./context/context";
+import "./styles/globalStyles.css";
 
 function App() {
-
     return (
-        <GameProvider>
-            <main>
-                <div className="grid grid-cols-4  justify-between items-center ">
-                    <Sidebar/>
-                    <div className="col-span-2 flex flex-col h-screen justify-center  bg-gray-500">
-                        <List type={"Ground"}/>
-                        <List type={"Hand"}/>
+        <React.StrictMode>
+            <GameProvider>
+                <main>
+                    <div className="grid grid-cols-4  justify-between items-center ">
+                        <Sidebar/>
+                        <div className="col-span-2 flex flex-col h-screen justify-center  bg-gray-500">
+                            <List type={"Ground"}/>
+                            <List type={"Hand"}/>
+                        </div>
+                        <Hero/>
                     </div>
-                    <Hero/>
-                </div>
-            </main>
-        </GameProvider>
+                </main>
+            </GameProvider>
+        </React.StrictMode>
     );
 }
-
-export default App;
 
 ReactDOM.render(<App/>, document.getElementById("root"));
