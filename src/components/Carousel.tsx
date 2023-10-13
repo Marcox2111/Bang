fc9c284f3f65eb96dc9a5e0b45d11fe4cbbc43f4
Marco Pellegrino <marco.pellegrino@tii.ae>
2023-10-13 19:43:20 +0400

    Carosello finito (forse voglio aggiustare una cosa ma non ricordo cosa)

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,77 +1,73 @@
-import React, {useEffect} from 'react';
-import {Card} from "./card";
-import {CardType} from "../types";
-import {motion, PanInfo, useSpring} from 'framer-motion';
-
+import React, { useEffect, useRef } from 'react';
+import { Card } from "./card";
+import { CardType } from "../types";
+import { motion, PanInfo, useSpring } from 'framer-motion';
 
 type CarouselProps = {
     cards: CardType[];
     divHeight: number;
-}
+    divWidth: number;
+};
 
-export function Carousel({cards, divHeight}: CarouselProps) {
+export function Carousel({ cards, divHeight, divWidth }: CarouselProps) {
+    // Constants and calculations related to the carousel layout
     const totalCards = cards.length;
     const maxCards = 44;
-    const rotInc = 2*Math.PI / maxCards;
-    const minRotation = (0.8 - Math.floor(totalCards / 2)) * rotInc * 180 / Math.PI;
-    const maxRotation = (Math.floor(totalCards / 2) - 0.8) * rotInc * 180 / Math.PI;
-    const CardHeight = divHeight/1.1;
-    const CardWidth = CardHeight/1.556;
-    const CarouselDiameter = 1.7*(CardHeight + maxCards*CardWidth/(2*Math.PI));
+    const rotInc = 2 * Math.PI / maxCards;
+    const CardHeight = divHeight / 1.1;
+    const CardWidth = CardHeight / 1.556;
+    const CarouselDiameter = 1.7 * (CardHeight + maxCards * CardWidth / (2 * Math.PI));
+    const divWidthAngle = 2 * Math.asin(divWidth / CarouselDiameter) * 180 / Math.PI;
+    const lastCardAngle = rotInc * Math.floor(totalCards / 2) * 180 / Math.PI;
+    const maxRotation = Math.abs(divWidthAngle / 2 - lastCardAngle - 5);
+    const minRotation = -Math.abs(divWidthAngle / 2 - lastCardAngle - 5);
+
+    // State and refs
     const rotation = useSpring(0, { stiffness: 100, damping: 12 });
+    const rotationRef = useRef(0);
 
     useEffect(() => {
-        rotation.set(0); // Set initial value
-    }, []); // Empty dependency array means this useEffect runs once on mount
+        rotation.set(0);
+    }, [rotation]);
 
-
-    function calculateTransformations (index: number) {
+    const calculateTransformations = (index: number) => {
         const adjustedIndex = index - Math.floor(totalCards / 2);
         const translateX = (CarouselDiameter / 2 - CardWidth / 1.9) * Math.cos(rotInc * adjustedIndex - Math.PI / 2);
         const translateY = (CarouselDiameter / 2 - CardHeight / 1.9) * Math.sin(rotInc * adjustedIndex - Math.PI / 2);
         const rotationRad = rotInc * adjustedIndex;
 
-        return {
-            translateX,
-            translateY,
-            rotationRad,
-        };
-    }
-
-    const {
-        translateX: initialX,
-        translateY: initialY,
-        rotationRad: initialAngle
-    } = calculateTransformations(Math.floor(totalCards / 2));
-
-
-    const onPan = (event: PointerEvent, info: PanInfo) => {
-        const newRotation = info.offset.x / 10; // Adjust this calculation
-        const clampedRotation = Math.max(minRotation, Math.min(maxRotation, newRotation));
-        rotation.set(clampedRotation); // Use rotation.set() to update the rotation value
+        return { translateX, translateY, rotationRad };
     };
 
+    const onPan = (event: PointerEvent, info: PanInfo) => {
+        const newRotation = rotationRef.current + info.offset.x / 10;
+        const clampedRotation = newRotation < 0
+            ? Math.max(newRotation, minRotation)
+            : Math.min(newRotation, maxRotation);
+        rotation.set(clampedRotation*0.95);
+    };
+
+    const { translateX: initialX, translateY: initialY, rotationRad: initialAngle } = calculateTransformations(Math.floor(totalCards / 2));
 
     return (
         <motion.div
             className="circular"
             onPan={onPan}
-            style={{
-                width: `${CarouselDiameter}px`,
-                rotate: rotation
-            }}>
+            onPanEnd={() => rotationRef.current = rotation.get()}
+            style={{ width: `${CarouselDiameter}px`, rotate: rotation }}
+        >
             {cards.map((card, index) => {
-                const {translateX, translateY, rotationRad} = calculateTransformations(index);
+                const { translateX, translateY, rotationRad } = calculateTransformations(index);
                 return (
                     <motion.div
                         key={card.id}
                         className="card"
-                        initial={{transform: `translate(${initialX}px, ${initialY}px) rotate(${initialAngle}rad)`}}
-                        animate={{transform: `translate(${translateX}px, ${translateY}px) rotate(${rotationRad}rad)`}}
-                        transition={{duration: 0.5}}
+                        initial={{ transform: `translate(${initialX}px, ${initialY}px) rotate(${initialAngle}rad)` }}
+                        animate={{ transform: `translate(${translateX}px, ${translateY}px) rotate(${rotationRad}rad)` }}
+                        transition={{ duration: 0.5 }}
                         whileHover={{
                             transform: `translate(${translateX}px, ${translateY}px) rotate(${rotationRad}rad) scale(1.05)`,
-                            transition: {duration: 0.32},
+                            transition: { duration: 0.32 },
                         }}
                         style={{
                             width: `${CardWidth}px`,
@@ -79,7 +75,7 @@ export function Carousel({cards, divHeight}: CarouselProps) {
                             transform: `translate(${translateX}px, ${translateY}px) rotate(${rotationRad}rad)`
                         }}
                     >
-                        <Card id={card.id} cardName={card.title}/>
+                        <Card id={card.id} cardName={card.title} />
                     </motion.div>
                 );
             })}
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,23 +3,39 @@ import {useGame} from "../context/Context";
 import Carousel from "./Carousel";
 import {useMediaQuery} from 'react-responsive';
 import {useResizeDetector} from "react-resize-detector";
+import { motion, PanInfo, useSpring } from 'framer-motion';
 
 
 export function Game() {
     const isSmallScreen = useMediaQuery({maxWidth: 640});
 
-    const {activePlayerID, players} = useGame();
+    const {activePlayerID,setActivePlayerID, players,setPlayers} = useGame();
     const {height: carHeight,ref:carRef} = useResizeDetector();
+    const {width: containerWidth,ref:containerDiv} = useResizeDetector();
 
+    if (players[1].Hand.length > 4) {
+        const newPlayers = [...players]; // Create a shallow copy of the players array
+        newPlayers[1].Hand = newPlayers[1].Hand.slice(0, -1
+        ); // Remove the last card from player 1's hand
+        setPlayers(newPlayers); // Update the state with the new players array
+    }
+
+    function scrollPlayer(event: PointerEvent, info: PanInfo) {
+
+        console.log(info.velocity.x);
+        const nextPlayerID = info.velocity.x > 0 ? activePlayerID - 1 : activePlayerID + 1;
+        setActivePlayerID((nextPlayerID + players.length) % players.length);
+
+    }
 
     return (
         <div
-            className="flex flex-col justify-between h-screen w-screen overflow-hidden p-0 m-0 sm:p-4 sm:min-h-screen sm:items-center sm:justify-center">
-            <div
+            className="flex flex-col justify-between h-screen w-screen overflow-hidden p-0 m-0 sm:p-4 sm:min-h-screen sm:items-center sm:justify-center touch-none">
+            <div ref={containerDiv}
                 className="flex flex-col justify-between items-center h-full w-full sm:shadow-2xl sm:rounded-xl max-w-full">
-                <div className="text-2xl font-bold mt-4 mb-4">Game</div>
+                <motion.div onPanEnd={scrollPlayer} className="text-2xl font-bold mt-4 mb-4">{players[activePlayerID].name}</motion.div>
                 <div ref={carRef} className="basis-1/3 overflow-hidden sm:basis-1/4">
-                    {carHeight||0 > 0 && <Carousel divHeight={carHeight||0} cards={players[activePlayerID].Hand}/>}
+                    {carHeight > 0 && <Carousel divHeight={carHeight} divWidth={containerWidth} cards={players[activePlayerID].Hand}/>}
                 </div>
             </div>
         </div>
